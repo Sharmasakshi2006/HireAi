46eb89af8f1c28f1b0bb06e846375a98ae451221
sakshi <mudgalsakshi2005@gmail.com>
2026-05-14 14:04:27 +0530

    Switched to sqlite

--- backend/settings.py
+++ backend/settings.py
@@ -79,12 +79,8 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'hireai_db',
-        'USER': 'root',
-        'PASSWORD': 'sakshi123@456',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
